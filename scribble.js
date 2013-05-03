b93fa364a18861e24589d62ce5086720a2a92822
Sebastian Leu <sebastian.leu1@gmail.com>
2013-05-04 01:53:13 +0200

    added pages.inc and some todos

--- scribble.js
+++ scribble.js
@@ -5,7 +5,21 @@
    */
   Drupal.behaviors.createBlackboard = {}
   Drupal.behaviors.createBlackboard.attach = function (context, settings) {
-    $('.scribble-canvas').jqScribble();
+    // @todo get the path and filename from the Drupal.settings array
+    var options = {
+      backgroundImage: 'sites/all/libraries/jqscribble/index.png'
+    };
+    $('#scribble-canvas').jqScribble(options);
+
+    $('#scribble-save').click(function () {
+      $(".scribble-canvas").data("jqScribble").save(function (imageData) {
+        if(confirm("This will write a file using the example image_save.php. Is that cool with you?")) {
+          $.post(Drupal.settings.scribble.savePath, {imagedata: imageData}, function(response) {
+            // @todo print some message and reload the canvas with the new backgroundimage
+          });
+        }
+      });
+    });
   }
 
 })(jQuery);
