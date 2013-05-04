c0e7e83dcf3240d479d4610d6d5bed93c1143cad
Sebastian Leu <sebastian.leu1@gmail.com>
2013-05-04 16:20:49 +0200

    added JS settings, changed JS

--- scribble.js
+++ scribble.js
@@ -5,20 +5,22 @@
    */
   Drupal.behaviors.createBlackboard = {}
   Drupal.behaviors.createBlackboard.attach = function (context, settings) {
-    // @todo get the path and filename from the Drupal.settings array
-    var options = {
-      backgroundImage: 'sites/all/libraries/jqscribble/index.png'
-    };
-    $('#scribble-canvas').jqScribble(options);
 
-    $('#scribble-save').click(function () {
+    var options = {
+      backgroundImage: Drupal.settings.scribble.bgImagePath + '/index.png'
+    };
+    $('.scribble-canvas').jqScribble(options);
+
+    $('.scribble-save').click(function () {
       $(".scribble-canvas").data("jqScribble").save(function (imageData) {
-        if(confirm("This will write a file using the example image_save.php. Is that cool with you?")) {
-          $.post(Drupal.settings.scribble.savePath, {imagedata: imageData}, function(response) {
+        if(confirm("You're about to save ur changes. Is that cool with you?")) {
+          $.post(Drupal.settings.scribble.saveURL, {imagedata: imageData}, function(response) {
             // @todo print some message and reload the canvas with the new backgroundimage
+            console.log('RETURND FROM AJAX');
           });
         }
       });
+      return FALSE;
     });
   }
 
