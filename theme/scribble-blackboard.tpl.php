b3f5cafd7232cbf2a594db6e6327a88e268d27e8
Sebastian Leu <sebastian.leu1@gmail.com>
2013-05-09 16:02:40 +0200

    added t() calls in template, added clear action

--- theme/scribble-blackboard.tpl.php
+++ theme/scribble-blackboard.tpl.php
@@ -1,19 +1,21 @@
 <?php
 /**
- *
+ * Template for the scribble blackboard.
+ * // @todo make brushed configurable.
  */
+
 ?>
 <div class="scribble-blackboard-wrapper">
   <canvas class="scribble-canvas"></canvas>
   <div class="scribble-actions">
     <a href="#" class="scribble-save"><?php print t('Save'); ?></a>
-    <a href="#" class="scribble-add">Add Image</a>
-    <a href="#" class="scribble-clear">Clear</a>
+    <a href="#" class="scribble-add"><?php print t('Add Image'); ?></a>
+    <a href="#" class="scribble-clear"><?php print t('Clear'); ?></a>
   </div>
   <div class="scribble-brushes">
-    <strong>BRUSHES:</strong>
-    <a href="#" class="scribble-brush-btn" rel="basic">Basic</a>
-    <a href="#" class="scribble-brush-btn" rel="line">Line</a>
-    <a href="#" class="scribble-brush-btn" rel="cross">Cross</a>
+    <strong><?php print t('Brushes'); ?></strong>
+    <a href="#" class="scribble-brush-btn" rel="basic"><?php print t('Basic'); ?></a>
+    <a href="#" class="scribble-brush-btn" rel="line"><?php print t('Line'); ?></a>
+    <a href="#" class="scribble-brush-btn" rel="cross"><?php print t('Cross'); ?></a>
   </div>
 </div>
