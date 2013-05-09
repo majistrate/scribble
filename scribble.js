2592456985d20ab1ff359777e0e43771f4fc457c
Sebastian Leu <sebastian.leu1@gmail.com>
2013-05-09 23:42:05 +0200

    added add img action

--- scribble.js
+++ scribble.js
@@ -1,5 +1,6 @@
 (function($) {
 //@todo make the canvas class configurable in the UI and it add as var to JS settings
+//@todo make the filename dynamic and save it in the form as hidden element, adapt filename 'scribble.png' here accordingly
   /**
    * Behavior for scribble module.
    */
@@ -15,9 +16,9 @@
     // Register the handler for the save action.
     $('.scribble-save').click(function () {
       $(".scribble-canvas").data("jqScribble").save(function (imageData) {
-        if(confirm("You're about to save ur changes. Is that cool with you?")) {
+        if(confirm(Drupal.t('You\'re about to save ur changes. Is that cool with you?'))) {
           $.post(Drupal.settings.scribble.saveURL, {imagedata: imageData}, function(response) {
-            console.log('RETURNED FROM AJAX');
+console.log('RETURNED FROM SAVE AJAX');
             var options = {
               backgroundImage: Drupal.settings.scribble.bgImagePath + '/scribble.png'
             };
@@ -33,6 +34,8 @@
       'cross': CrossBrush,
       'basic': BasicBrush
     };
+
+    var add_image_dragged = false;
 
     // Register brush handlers
     $('.scribble-brush-btn').click(function () {
@@ -50,6 +53,48 @@
       $('.scribble-canvas').data('jqScribble').update(options);
       return false;
     });
+
+    // Handle image add action.
+    $('.scribble-add').click(function () {
+      console.log(event);
+      var img_src = prompt(Drupal.t('Enter the URL of the image.');
+      if(img_src !== '') {
+        var $info = $('<div>' + Drupal.t('Drag the image on the blackboard in order to add it.') + '</div>');
+        $('.scribble-add-img-container').html($info);
+        var $img = $('<img />');
+        $img.attr({
+          'src': img_src,
+          'class': 'scribble-add-image'
+        });
+        $('.scribble-add-img-container').append($img);
+        var options = {
+          stop: addImgStopHandler,
+          revert: true
+        };
+        $img.draggable(options);
+      }
+      return false;
+    });
+
+    function addImgStopHandler(event, ui) {
+      // @todo calculate mouse offset from dragged img into x and y
+      // @todo check if the img was dropped within the canvas dimensions
+      var x = event.pageX - $('.scribble-canvas').offset().left;
+      var y = event.pageY - $('.scribble-canvas').offset().top;
+      var data = {
+        img_url: $('.scribble-add-image').attr('src'),
+        img_width: $('.scribble-add-image').width(),
+        img_height: $('.scribble-add-image').height(),
+        dst_x: x,
+        dst_y: y
+      };
+      $.post(Drupal.settings.scribble.addURL, data, function(response) {
+        var options = {
+          backgroundImage: Drupal.settings.scribble.bgImagePath + '/scribble.png'
+        };
+        $('.scribble-canvas').data('jqScribble').update(options);
+      });
+    }
   }
 
 })(jQuery);
--- theme/scribble-blackboard.tpl.php
+++ theme/scribble-blackboard.tpl.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * Template for the scribble blackboard.
- * // @todo make brushed configurable.
+ * @todo make brushes configurable and loop them.
  */
 
 ?>
 <div class="scribble-blackboard-wrapper">
   <canvas class="scribble-canvas"></canvas>
   <div class="scribble-actions">
+    <strong><?php print t('Actions'); ?></strong>
     <a href="#" class="scribble-save"><?php print t('Save'); ?></a>
     <a href="#" class="scribble-add"><?php print t('Add Image'); ?></a>
     <a href="#" class="scribble-clear"><?php print t('Clear'); ?></a>
@@ -18,4 +19,5 @@
     <a href="#" class="scribble-brush-btn" rel="line"><?php print t('Line'); ?></a>
     <a href="#" class="scribble-brush-btn" rel="cross"><?php print t('Cross'); ?></a>
   </div>
+  <div class="scribble-add-img-container"></div>
 </div>
