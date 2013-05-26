6d4d1e2ed8307b1cd0e4e65ff7cbfaf41ce195d4
Sebastian Leu <s.leu@gmx.net>
2013-05-26 03:51:18 +0200

    buttons for toolbar, rearranged widgets, added some CSS

--- theme/scribble-blackboard.tpl.php
+++ theme/scribble-blackboard.tpl.php
@@ -7,19 +7,21 @@
 ?>
 <div class="scribble-blackboard-wrapper">
   <canvas class="scribble-canvas"></canvas>
-  <div class="scribble-actions">
-    <strong><?php print t('Actions'); ?></strong>
-    <a href="#" class="scribble-save"><?php print t('Save'); ?></a>
-    <a href="#" class="scribble-add"><?php print t('Add Image'); ?></a>
-    <a href="#" class="scribble-clear"><?php print t('Clear'); ?></a>
+  <div class="scribble-toolbar ui-widget-header ui-corner-all">
+    <div class="scribble-actions">
+      <a href="#" class="scribble-save"><?php print t('Save'); ?></a>
+      <a href="#" class="scribble-add"><?php print t('Add Image'); ?></a>
+      <a href="#" class="scribble-clear"><?php print t('Clear'); ?></a>
+      <div class="scribble-color-btn"><?php print t('Color'); ?></div>
+      <div class="scribble-color-display ui-corner-all"></div>
+      <div class="scribble-color-picker"></div>
+      <div class="scribble-brush-size"></div>
+    </div>
+    <div class="scribble-brushes">
+      <input type="radio" id="basic" name="brushes-btns" checked="checked" /><label for="basic"><?php print t('Basic'); ?></label>
+      <input type="radio" id="line" name="brushes-btns" /><label for="line"><?php print t('Line'); ?></label>
+      <input type="radio" id="cross" name="brushes-btns" /><label for="cross"><?php print t('Cross'); ?></label>
+    </div>
+    <div class="scribble-add-img-container"></div>
   </div>
-  <div class="scribble-brushes">
-    <strong><?php print t('Brushes'); ?></strong>
-    <a href="#" class="scribble-brush-btn" rel="basic"><?php print t('Basic'); ?></a>
-    <a href="#" class="scribble-brush-btn" rel="line"><?php print t('Line'); ?></a>
-    <a href="#" class="scribble-brush-btn" rel="cross"><?php print t('Cross'); ?></a>
-  </div>
-  <div class="scribble-brush-size"></div>
-  <div class="scribble-color-picker"></div>
-  <div class="scribble-add-img-container"></div>
 </div>
