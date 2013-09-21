0c2c30c5d8fd90c1b11f155a4ec191b8671b7575
Sebastian Leu <s.leu@gmx.net>
2013-09-21 22:33:37 +0200

    Added and implemented view callback, implemented buildContent() method

--- theme/scribble-blackboard.tpl.php
+++ theme/scribble-blackboard.tpl.php
@@ -6,30 +6,28 @@
  */
 
 ?>
-<div class="scribble-blackboard-wrapper">
-  <div class="scribble-toolbar ui-widget-header ui-corner-all">
-    <div class="scribble-actions">
-      <div class="scribble-save"><?php print t('Save'); ?></div>
-      <div class="scribble-clear"><?php print t('Clear'); ?></div>
-      <input type="text" value="http://www.istos.it/sites/default/files/istoslogo.png" class="ui-corner-all" id="img-src-txt" name="scribble-img-src" placeholder="<?php print t('Enter the URL of the image to add'); ?>" />
-      <div class="scribble-add"><?php print t('Add Image'); ?></div>
-    </div>
-    <hr class="scribble-separator" />
-    <div class="scribble-brush-settings">
-      <div class="scribble-color-btn"><?php print t('Color'); ?></div>
-      <div class="scribble-color-display ui-corner-all"></div>
-      <div class="scribble-color-picker"></div>
-      <div class="scribble-brush-size"></div>
-      <div class="scribble-brush-size-display ui-corner-all"></div>
-      <div class="scribble-brushes">
-        <input type="radio" id="basic" name="brushes-btns" checked="checked" /><label for="basic"><?php print t('Basic'); ?></label>
-        <input type="radio" id="line" name="brushes-btns" /><label for="line"><?php print t('Line'); ?></label>
-        <input type="radio" id="cross" name="brushes-btns" /><label for="cross"><?php print t('Cross'); ?></label>
-      </div>
-    </div>
-    <div class="scribble-add-img-container"></div>
+<div class="scribble-toolbar ui-widget-header ui-corner-all">
+  <div class="scribble-actions">
+    <div class="scribble-save"><?php print t('Save'); ?></div>
+    <div class="scribble-clear"><?php print t('Clear'); ?></div>
+    <input type="text" value="http://www.istos.it/sites/default/files/istoslogo.png" class="ui-corner-all" id="img-src-txt" name="scribble-img-src" placeholder="<?php print t('Enter the URL of the image to add'); ?>" />
+    <div class="scribble-add"><?php print t('Add Image'); ?></div>
   </div>
-  <div class="scribble-canvas-wrapper">
-    <canvas class="scribble-canvas" />
+  <hr class="scribble-separator" />
+  <div class="scribble-brush-settings">
+    <div class="scribble-color-btn"><?php print t('Color'); ?></div>
+    <div class="scribble-color-display ui-corner-all"></div>
+    <div class="scribble-color-picker"></div>
+    <div class="scribble-brush-size"></div>
+    <div class="scribble-brush-size-display ui-corner-all"></div>
+    <div class="scribble-brushes">
+      <input type="radio" id="basic" name="brushes-btns" checked="checked" /><label for="basic"><?php print t('Basic'); ?></label>
+      <input type="radio" id="line" name="brushes-btns" /><label for="line"><?php print t('Line'); ?></label>
+      <input type="radio" id="cross" name="brushes-btns" /><label for="cross"><?php print t('Cross'); ?></label>
+    </div>
   </div>
+  <div class="scribble-add-img-container"></div>
+</div>
+<div class="scribble-canvas-wrapper">
+  <canvas class="scribble-canvas" />
 </div>
